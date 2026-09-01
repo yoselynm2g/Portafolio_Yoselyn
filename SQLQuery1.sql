be08e5b18b127041b5665f9014c8889c673aefcd
Yoselyn Mogollón <yoselynm2g@gmail.com>
2026-09-01 12:54:41 -0400

    Delete SQLQuery1.sql

--- SQLQuery1.sql
+++
@@ -1,6 +0,0 @@
--- Duplicados reales (mismo pedido + mismo producto repetido)
-
-SELECT Order_ID, Product_ID, COUNT(*) AS veces_repetido
-FROM ecommerce_orders_dataset
-GROUP BY Order_ID, Product_ID
-HAVING COUNT(*) > 1;
