62ce00a1fd09e531f86f7d817ff51ab84c220ba1
Yoselyn Mogollón <yoselynm2g@gmail.com>
2026-09-01 12:56:35 -0400

    Delete SQLQuery5.sql

--- SQLQuery5.sql
+++
@@ -1,9 +0,0 @@
-
---NULLs en columnas clave
-
-SELECT 
-  COUNT(CASE WHEN Customer_Age IS NULL THEN 1 END) AS nulos_edad,
-  COUNT(CASE WHEN Customer_Gender IS NULL THEN 1 END) AS nulos_genero,
-  COUNT(CASE WHEN City IS NULL THEN 1 END) AS nulos_ciudad,
-  COUNT(CASE WHEN Unit_Price IS NULL THEN 1 END) AS nulos_precio
-FROM ecommerce_orders_dataset;
